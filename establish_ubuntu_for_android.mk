25b6205aec8e7ac53a7974c2d1db8cb17ea9357e
Kevin Nuñez Banderas <kn06497@hotmail.com>
2016-06-20 19:56:20 -0500

    Create establish_ubuntu_for_android.mk

---
+++ establish_ubuntu_for_android.mk
@@ -0,0 +1,3 @@
+# Leaning_git
+
+sudo apt-get install openjdk-7-jdk git ccache automake lzop bison gperf build-essential zip curl zlib1g-dev zlib1g-dev:i386 g++-multilib python-networkx libxml2-utils bzip2 libbz2-dev libbz2-1.0 libghc-bzlib-dev squashfs-tools pngcrush schedtool dpkg-dev liblz4-tool make optipng maven
